a4bf051ee3adb65f8b3e4eb19eaaeb0f9696ade1
kavindu Jayawardhana <kavindhujayawardhana@gmail.com>
2026-05-12 07:45:20 +0530

    added backend routine to chatbot

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './i18n';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
